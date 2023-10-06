54cef34e5503ceac8032816e4860ad31fe567c84
richardsochima <richardsochima>
2023-10-06 10:52:47 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@ int main (void)
 	long int c;
 	long long int d;
 	float f;
-
 printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %1u bytes(s)\n", (unsigned long)sizeof(c));
