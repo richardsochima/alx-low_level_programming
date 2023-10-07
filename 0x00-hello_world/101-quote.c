bde6bf05992382e1f286ffca67737adc56e55544
richardsochima <richardsochima>
2023-10-07 01:50:41 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * main - print a message to stderr
+ * Return: 0
+ */
+int main(void)
+{
+char *error = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, error, 59);
+	return (1);
+}
