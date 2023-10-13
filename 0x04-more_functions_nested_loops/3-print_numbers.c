ea1765b57a113c713a3a70ed307f9f87f6a2da14
richardsochima <richardsochima>
2023-10-13 08:10:56 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_numbers -Print the number from 0 to 9
+ *Return: The number from 0 to 0
+*/
+void print_numbers(void)
+
+{
+int a;
+for (a = 0; a <= 9; a++)
+_putchar(a + '0');
+_putchar ('\n');
+}
