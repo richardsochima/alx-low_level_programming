7c0acc8b1183f681a2f346f7937beab57887ca9f
richardsochima <richardsochima>
2023-11-01 13:03:55 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print program name
+ * @argc: argument count
+ * @argv: arguement vector
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
