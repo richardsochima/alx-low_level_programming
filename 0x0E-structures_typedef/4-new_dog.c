9bbd764ee6764e457eb4cb063c610b1f8a0a58f0
richardsochima <richardsochima>
2023-11-06 17:57:10 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,83 +1,50 @@
-#include <stdio.h>
+#include "dog.h"
 #include <stdlib.h>
 #include "dog.h"
+
 /**
- * new_dog - creates a new dog
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- *
- * Return: pointer to the new dog (Success), NULL otherwise
+ * new_dog - create a new dog
+ * @name: char string name
+ * @age: int age
+ * @owner: char string owner
+ * Return: pointer to new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	int length1, length2;
+	struct dog *doge;
+	int i, j, k;
+	char *n, *o;
 
-	length1 = _strlen(name);
-	length2 = _strlen(owner);
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	doge = malloc(sizeof(struct dog));
+	if (doge == NULL)
 		return (NULL);
-	dog->name = malloc(sizeof(char) * (length1 + 1));
-	if (dog->name == NULL)
+
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (j = 0; owner[j] != '\0'; j++)
+		;
+
+	n = malloc(sizeof(char) * i + 1);
+	if (n == NULL)
 	{
-		free(dog);
-		return (NULL);
-	}
-	dog->owner = malloc(sizeof(char) * (length2 + 1));
-	if (dog->owner == NULL)
-	{
-		free(dog);
-		free(dog->name);
+		free(doge);
 		return (NULL);
 	}
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
-	dog->age = age;
-	return (dog);
-}
-
-/**
- * *_strcpy - copies the string pointed to by srcpy including the
- * terminating null byte (\0) to the buffer pointed to by dest
- * @dest: pointer to the buffer in which to copy the string
- * @src: string copied
- *
- * Return: the pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int length, j;
-
-	length = 0;
-
-	while (src[length] != '\0')
+	o = malloc(sizeof(char) * j + 1);
+	if (o == NULL)
 	{
-		length++;
+		free(n);
+		free(doge);
+		return (NULL);
 	}
-	for (j = 0; j < length; j++)
-	{
-		dest[j] = src[j];
-	}
-	dest[j] = '\0';
-	return (dest);
-}
+	for (k = 0; k <= i; k++)
+		n[k] = name[k];
+	for (k = 0; k <= j; k++)
+		o[k] = owner[k];
 
-/**
- * * _strlen - returns the length of a string
- * @s: string to be evaluated
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int j;
+	doge->name = n;
+	doge->age = age;
+	doge->owner = o;
 
-	j = 0;
-	while (s[j] != '\0')
-	{
-		j++;
-	}
-	return (j);
+	return (doge);
 }
